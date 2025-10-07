3b9b181096cd71c4aa06cf62ce9fc5bc67715948
youzheyin <1298763831@qq.com>
2025-10-07 18:04:16 +0800

    [chore] change cli name into `crv`.

--- crv-cli/src/main.rs
+++ crv-cli/src/main.rs
@@ -2,7 +2,6 @@ use clap::{Parser, Subcommand};
 use crv_cli::client::CrvClient;
 
 #[derive(Parser)]
-#[command(name = "crv-cli")]
 #[command(about = "Chronoverse CLI - Command line interface for crv")]
 struct Cli {
     #[command(subcommand)]
@@ -26,10 +25,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     match cli.command {
         Commands::Connect { server } => {
             println!("正在连接到服务器: {}", server);
-            
+
             let mut client = CrvClient::new(&server).await?;
             println!("连接成功！");
-            
+
             let response = client.bonjour().await?;
             println!("收到服务器信息:");
             println!("  守护进程版本: {}", response.daemon_version);
