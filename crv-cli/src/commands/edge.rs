720a3dbf7334ef9ec9d7e71023a7bc47d26616e9
youzheyin <1298763831@qq.com>
2025-12-20 23:09:06 +0800

    [feat] get runtime config cli.

--- crv-cli/src/commands/edge.rs
+++ crv-cli/src/commands/edge.rs
@@ -5,7 +5,7 @@ use clap::{Parser, Subcommand};
 use console::{Emoji, style};
 use crv_edge::{
     daemon_server::config::BootstrapConfig,
-    pb::{BonjourReq, system_service_client::SystemServiceClient},
+    pb::{BonjourReq, GetRuntimeConfigReq, system_service_client::SystemServiceClient},
 };
 use tonic::transport::Channel;
 
@@ -21,6 +21,7 @@ impl EdgeCli {
         match &self.edge_commands {
             EdgeCommands::Bonjour(bonjour) => bonjour.handle(channel).await,
             EdgeCommands::BootstrapConfig(bootstrap_config) => bootstrap_config.handle().await,
+            EdgeCommands::RuntimeConfig(runtime_config) => runtime_config.handle(channel).await,
         }
     }
 }
@@ -29,6 +30,7 @@ impl EdgeCli {
 pub enum EdgeCommands {
     Bonjour(BonjourCli),
     BootstrapConfig(BootstrapConfigCli),
+    RuntimeConfig(RuntimeConfigCli),
 }
 
 #[derive(Parser)]
@@ -80,6 +82,58 @@ impl BootstrapConfigCli {
         );
 
         // 2. 展示设置项部分
+        println!("{}", style("SETTINGS").bold().dim());
+
+        // 计算 Key 的最大长度以实现对齐
+        let max_key_len = settings.keys().map(|k| k.len()).max().unwrap_or(0);
+
+        for (key, value) in settings {
+            println!(
+                " {} {:width$} {} {}",
+                Emoji("●", "*"),       // 前缀小圆点
+                style(key).cyan(),     // Key 颜色
+                style("→").dim(),      // 箭头符号
+                style(value).yellow(), // Value 颜色
+                width = max_key_len    // 动态填充宽度
+            );
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Parser)]
+#[command(about = "Show runtime config.", long_about = None)]
+pub struct RuntimeConfigCli;
+
+impl RuntimeConfigCli {
+    pub async fn handle(&self, channel: &Channel) -> Result<()> {
+        let mut client = SystemServiceClient::new(channel.clone());
+        let runtime_config = client
+            .get_runtime_config(GetRuntimeConfigReq {})
+            .await?
+            .into_inner();
+
+        let mut settings = BTreeMap::new();
+        if let Some(item) = runtime_config.remote_addr {
+            settings.insert("HIVE_PORT", format!("\"{}\"({})", item.value, item.source));
+        }
+        if let Some(item) = runtime_config.editor {
+            settings.insert(
+                "EDITOR_COMMAND",
+                format!("\"{}\"({})", item.value, item.source),
+            );
+        }
+        if let Some(item) = runtime_config.user {
+            settings.insert("USER", format!("\"{}\"({})", item.value, item.source));
+        }
+
+        println!(
+            "\n{}\n",
+            style(" ⚙  Configuration Details ").bold().reverse().cyan()
+        );
+
+        // 1. 展示设置项部分
         println!("{}", style("SETTINGS").bold().dim());
 
         // 计算 Key 的最大长度以实现对齐
