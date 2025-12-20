3f49d996694d50c585a0f3ce47fd67528a2795f3
youzheyin <1298763831@qq.com>
2025-12-20 15:21:15 +0800

    [feat] command to show bootstrap config.

--- crv-cli/src/commands/edge.rs
+++ crv-cli/src/commands/edge.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
-use crv_edge::pb::{BonjourReq, system_service_client::SystemServiceClient};
+use crv_edge::{
+    daemon_server::config::BootstrapConfig,
+    pb::{BonjourReq, system_service_client::SystemServiceClient},
+};
 use tonic::transport::Channel;
 
 #[derive(Parser)]
@@ -14,6 +17,7 @@ impl EdgeCli {
     pub async fn handle(&self, channel: &Channel) -> Result<()> {
         match &self.edge_commands {
             EdgeCommands::Bonjour(bonjour) => bonjour.handle(channel).await,
+            EdgeCommands::BootstrapConfig(bootstrap_config) => bootstrap_config.handle().await,
         }
     }
 }
@@ -21,6 +25,7 @@ impl EdgeCli {
 #[derive(Subcommand)]
 pub enum EdgeCommands {
     Bonjour(BonjourCli),
+    BootstrapConfig(BootstrapConfigCli),
 }
 
 #[derive(Parser)]
@@ -31,6 +36,22 @@ impl BonjourCli {
         let mut system_client = SystemServiceClient::new(channel.clone());
         let response = system_client.bonjour(BonjourReq {}).await?;
         println!("{:?}", response.into_inner());
+        Ok(())
+    }
+}
+
+#[derive(Parser)]
+#[command(about = "Show bootstrap config.", long_about = None)]
+pub struct BootstrapConfigCli;
+
+impl BootstrapConfigCli {
+    pub async fn handle(&self) -> Result<()> {
+        let bootstrap_config = BootstrapConfig::load().expect("Can't load bootstrap config.");
+        println!("daemon_port:{}", bootstrap_config.daemon_port);
+        println!(
+            "embedded_database_root:{}",
+            bootstrap_config.embedded_database_root
+        );
         Ok(())
     }
 }
